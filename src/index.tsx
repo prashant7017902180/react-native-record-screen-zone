395ab892c087d6b1040630be0b67df501e2f1017
Nayan <nayan.choubey@webmobriltechnologies.com>
2023-04-24 15:37:50 +0530

    first sonar run

--- src/index.tsx
+++ src/index.tsx
@@ -20,10 +20,7 @@ type CleanRecord = () => void;
 
 const useComponentLayout = () => {
   const [layout, setLayout] = useState<LayoutRectangle>({
-    width: 0,
-    height: 0,
-    x: 0,
-    y: 0,
+   
   });
 
   const onLayout = useCallback((event: LayoutChangeEvent) => {
